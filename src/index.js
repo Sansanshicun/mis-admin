679ed6143a1d7a5f132c63f7155c537e1975aad0
荷官人 <h1606123746@163.com>
2023-02-24 10:47:47 +0800

    mis-admin

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/*StrictMode 严格模式  */}
+    <App />
+  </React.StrictMode>
+);
+
